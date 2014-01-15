207a1dcadf2d39c2076b114de0b03def7a741c6d
pduda <pduda@outlook.com>
2014-01-15 23:42:57 +0100

    dostep do bazy

--- src/pl/waw/pduda/database/Database.java
+++ src/pl/waw/pduda/database/Database.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class Database 
 {
-	static final String USER = "wedt";
+	static final String USER = "13777141_wedt";
 	static final String PASS = "wedt123";
 	static final String BASE = "jdbc:mysql://serwer1363634.home.pl/13777141_wedt?characterEncoding=UTF-8";
 	
